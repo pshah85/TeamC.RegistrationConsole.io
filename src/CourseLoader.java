09c38e5568490c5a07bc6cb395ca9ef4e20aca18
pshah85 <shah.prem85@gmail.com>
2016-03-02 09:23:51 -0800

    Update CourseLoader.java

--- src/CourseLoader.java
+++ src/CourseLoader.java
@@ -1,16 +1,20 @@
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Map;
 
 /**
- * Created by Shahp on 2/24/2016.
+ * Author: TEAM C: [PREMAL SHAH, HAMID ASGARI, MITA JAGAD, LISA GRADY]
+ *Class: I&C SCI_X460.10 (WINTER 2016/REG 00216/SEC 1) (Java Programming I)
+ *Instructor: KESHA SMITH
+ *FINAL TEAM PROJECT: REGISTRATION CONSOLE
+ *Email: Shah.Prem85@gmail.com
+ * Created by TEam C on 2/23/2016.
  */
-public class CourseLoader {
+
+class CourseLoader {
 
     public static Map<String, Course> loadCourses() {
         Map<String, Course> courses = new HashMap<>();
